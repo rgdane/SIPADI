58a8307356f4f241fef5a27faf699d86f60ef2c2
rgdane <regadanew1@gmail.com>
2025-05-25 20:35:49 +0700

    header visual adjustment -  frontend

--- frontend/src/components/AppHeader.jsx
+++ frontend/src/components/AppHeader.jsx
@@ -1,6 +1,8 @@
 import { Layout } from 'antd';
 import LogoutButton from '../elements/LogoutButton';
 import { useAuth } from '../services/AuthContext';
+import batik from '../assets/batik.jpg';
+
 
 const { Header } = Layout;
 
@@ -8,8 +10,17 @@ export default function AppHeader() {
     const { user } = useAuth();
 
     return (
-        <Header style={{ background: '#F8F0E5', display: 'flex', justifyContent: 'space-between', alignItems: 'center',}} >
-            <h3>Halo, {user?.name}</h3>
+        <Header style={{
+            background: '#F8F0E5',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            backgroundImage: `url(${batik})`,
+            backgroundRepeat: 'repeat',
+            backgroundSize: 'contain',
+            }}
+        >
+            <h3 style={{ color: '#fff'}}>Halo, {user?.name}</h3>
             <LogoutButton />
         </Header>
         
